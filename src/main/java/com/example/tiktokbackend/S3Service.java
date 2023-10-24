1abbcb99aab8fdfec1c34da2e6d3642375f1d92f
thanatadcs <thanatad.cs@gmail.com>
2023-10-24 23:12:11 +0700

    Add method for creating s3 presigned url

---
+++ src/main/java/com/example/tiktokbackend/S3Service.java
@@ -0,0 +1,49 @@
+package com.example.tiktokbackend;
+
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.net.URL;
+import java.time.Duration;
+import java.util.Map;
+
+public class S3Service {
+
+    private static S3Presigner presigner = S3Presigner.builder()
+            .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+            .region(Region.AP_SOUTHEAST_1)
+            .build();
+
+    /**
+     * Create a presigned URL for uploading with a PUT request.
+     *
+     * @param bucketName  - The name of the bucket.
+     * @param keyName     - The name of the object.
+     * @param contentType - The content type of the object.
+     * @param metadata    - The metadata to store with the object.
+     * @return - The presigned URL for an HTTP PUT.
+     * <p>
+     * Source: https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/examples-s3-presign.html
+     */
+    public static URL createPresignedUrl(String bucketName, String keyName, String contentType, Map<String, String> metadata) {
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(keyName)
+                .contentType(contentType)
+                .metadata(metadata)
+                .build();
+
+        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))  // The URL will expire in 10 minutes.
+                .putObjectRequest(objectRequest)
+                .build();
+
+        PresignedPutObjectRequest presignedRequest = presigner.presignPutObject(presignRequest);
+
+        return presignedRequest.url();
+    }
+}
--- src/main/java/com/example/tiktokbackend/TiktokBackendApplication.java
+++ src/main/java/com/example/tiktokbackend/TiktokBackendApplication.java
@@ -3,10 +3,14 @@ package com.example.tiktokbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URL;
+
 @SpringBootApplication
 public class TiktokBackendApplication {
-
     public static void main(String[] args) {
+        URL url = S3Service.createPresignedUrl("scalable-p2", "sai", null, null);
+        System.out.println(url);
+
         SpringApplication.run(TiktokBackendApplication.class, args);
     }
 
