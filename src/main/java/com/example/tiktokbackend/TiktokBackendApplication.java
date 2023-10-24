78137c8feef69bf667d9481dc70c5d13fb820cea
thanatadcs <thanatad.cs@gmail.com>
2023-10-24 20:28:34 +0700

    First commit

---
+++ src/main/java/com/example/tiktokbackend/TiktokBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiktokbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktokBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiktokBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tiktokbackend/TiktokBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiktokbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiktokBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
