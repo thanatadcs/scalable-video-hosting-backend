2510f5e1a8c93840aca39e9535568839f3a63004
thanatadcs <thanatad.cs@gmail.com>
2023-10-27 00:11:17 +0700

    Add database support for docker compose

--- src/test/java/com/example/tiktokbackend/TiktokBackendApplicationTests.java
+++ src/test/java/com/example/tiktokbackend/TiktokBackendApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.tiktokbackend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TiktokBackendApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
