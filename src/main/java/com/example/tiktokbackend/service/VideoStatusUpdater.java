896ae4319e689497c2dbc26c2e42dba6aed807bb
thanatadcs <thanatad.cs@gmail.com>
2023-12-03 03:45:01 +0700

    Implement video status update logic in VideoStatusUpdater

--- src/main/java/com/example/tiktokbackend/service/VideoStatusUpdater.java
+++ src/main/java/com/example/tiktokbackend/service/VideoStatusUpdater.java
@@ -1,5 +1,7 @@
 package com.example.tiktokbackend.service;
 
+import com.example.tiktokbackend.domain.Video;
+import com.example.tiktokbackend.repository.VideoRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
@@ -10,11 +12,13 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class VideoStatusUpdater implements ApplicationListener<ApplicationReadyEvent> {
 
+    private VideoRepository videoRepository;
     private TaskQueueService taskQueueService;
     private final String QueueName = "backend";
 
-    VideoStatusUpdater(TaskQueueService taskQueueService) {
+    VideoStatusUpdater(TaskQueueService taskQueueService, VideoRepository videoRepository) {
         this.taskQueueService = taskQueueService;
+        this.videoRepository = videoRepository;
     }
 
     @Override
@@ -25,8 +29,15 @@ public class VideoStatusUpdater implements ApplicationListener<ApplicationReadyE
     public void updateVideoProcessingStatus() {
         while (true) {
             try {
-                String taskName = taskQueueService.getTask(QueueName);
-                log.info(taskName);
+                String message = taskQueueService.getTask(QueueName);
+                String[] splitMessage = message.split(",");
+                String taskName = splitMessage[0];
+                String uuid = splitMessage[1];
+                Video video = videoRepository.findByUuid(uuid);
+                if (taskName.equals("convert")) video.setConvertStatus("done");
+                else if (taskName.equals("thumbnail")) video.setThumbnailStatus("done");
+                else if (taskName.equals("chunk")) video.setChunkStatus("done");
+                videoRepository.save(video);
             } catch (Exception e) {
                 log.error(e.getMessage());
             }
