245263bbd60e270bb654197f549d70470fc7fdf8
thanatadcs <thanatad.cs@gmail.com>
2023-12-04 16:07:07 +0700

    Add ready_status column to Video entity

--- src/main/java/com/example/tiktokbackend/domain/Video.java
+++ src/main/java/com/example/tiktokbackend/domain/Video.java
@@ -17,15 +17,21 @@ public class Video {
     private String convertStatus;
     private String thumbnailStatus;
     private String chunkStatus;
+    private String readyStatus;
 
     public Video() {
     }
 
-    public Video(String uuid, String convertStatus, String thumbnailStatus, String chunkStatus) {
+    public Video(String uuid) {
+        this(uuid, null, null, null, null);
+    }
+
+    public Video(String uuid, String convertStatus, String thumbnailStatus, String chunkStatus, String readyStatus) {
         this.uuid = uuid;
         this.convertStatus = convertStatus;
         this.thumbnailStatus = thumbnailStatus;
         this.chunkStatus = chunkStatus;
+        this.readyStatus = readyStatus;
     }
 
     public Long getId() {
